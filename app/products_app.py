7e79109a96670b664427d2be523f12837cedb2fd
Alberts <jalberts@deloitte.com>
2017-07-17 19:15:18 -0400

    directory setup

---
+++ app/products_app.py
@@ -0,0 +1,30 @@
+import.csv
+
+# Write to CSV File
+#csv_file_path = #file name here
+
+#with open(csv_file_path, "w") as csv_file:
+    #writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
+    #writer.writeheader() # uses fieldnames set above
+    #writer.writerow({"city": "New York", "name": "Yankees"})
+    #writer.writerow({"city": "New York", "name": "Mets"})
+    #writer.writerow({"city": "Boston", "name": "Red Sox"})
+    #writer.writerow({"city": "New Haven", "name": "Ravens"})
+
+# Read CSV File
+#csv_file_path = "teams.csv"
+
+#with open(csv_file_path, "r") as csv_file:
+    #reader = csv.DictReader(csv_file) # assuming your CSV has headers, otherwise... csv.reader(csv_file)
+    #for row in reader:
+        #print(row["city"], row["name"])
+
+
+
+
+
+#Project Requirements
+#Contains a README.md file which provides instructions for how to install and use the program.
+#Contains a app/products_app.py file which represents the Python program.
+#Contains an empty data directory except for presence of a data/.gitignore file (see setup instructions).
+#Contains a commit history which reflects incremental development progress, perhaps corresponding with the checkpoint steps.
