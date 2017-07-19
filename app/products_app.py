26b1cebe2a8aedb58389d64ed93c3959fb6a8a29
Alberts <jalberts@deloitte.com>
2017-07-19 19:58:05 -0400

    Count the # of products

--- app/products_app.py
+++ app/products_app.py
@@ -1,6 +1,7 @@
 import csv
 csv_file_path = "data/products.csv"
 
+#Menu Greeting & Inputs
 print("----------------------------------")
 print("PRODUCTS APPLICATION")
 print("----------------------------------")
@@ -21,8 +22,11 @@ chosen_operation = input("Input Operation: ")
 chosen_operation = chosen_operation.title()
 print(chosen_operation)
 
-#Definitions
-def list_products():
+# Product List
+products = []
+
+#Menu Definitions
+def list_products(): #List Definition
         print("\n")
         print("List")
         print("THERE ARE 20 PRODUCTS:")
@@ -30,19 +34,20 @@ def list_products():
             reader = csv.DictReader(csv_file)
             for row in reader:
                 print(" + ", dict(row))
-def show_product():
-    print("SHOWING A PRODUCT")
-
-def create_product():
+def show_product(): #Show Definition
+    with open(csv_file_path,"r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            products.append(row)
+    print("There are " + str(len(products)) + " Products")
+def create_product(): #Create Definition
     print("CREATING A PRODUCT")
-
-def update_product():
+def update_product(): #Update Definition
     print("UPDATING A PRODUCT")
-
-def destroy_product():
+def destroy_product(): #Destroy Definition
     print("DESTROYING A PRODUCT")
 
-#If statements
+#Menu If statements
 if chosen_operation == "List": list_products()
 elif chosen_operation == "Show": show_product()
 elif chosen_operation == "Create": create_product()
