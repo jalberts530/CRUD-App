33fa28408fda2cc297e576ca560bf870c05d444d
Alberts <jalberts@deloitte.com>
2017-07-19 19:05:15 -0400

    Checkpt 1: Setup User interface

--- app/products_app.py
+++ app/products_app.py
@@ -1,9 +1,13 @@
+import csv
+csv_file_path = "data/products.csv"
+
+
 print("----------------------------------")
 print("PRODUCTS APPLICATION")
 print("----------------------------------")
-print("Welcome User")
-
-print("There are 21 Products in the database. Please select an operation:")
+print("Welcome User!")
+print("\n")
+print("There are 20 Products in the database. Please choose an operation:" )
 print("\n")
 print("    operation | description" )
 print("    --------- | -----------")
@@ -13,43 +17,19 @@ print("    'Create'  | Add a new produt.")
 print("    'Update'  | Edit an exisiting product.")
 print("    'Destroy' | Delete an exisiting product")
 
-operation = [List, Show, Create, Update, Destroy]
+print("\n")
+chosen_operation = input("Input Operation: ")
+print(chosen_operation)
 
-while True:
-    operation = input ("Please input a valid operation")
-        print(operation)
-        
+print("\n")
+print("List")
+print("THERE ARE 20 PRODUCTS:")
 
-
-
-
-#import.csv
-
-# Write to CSV File
-#csv_file_path = #file name here
-
-#with open(csv_file_path, "w") as csv_file:
-    #writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
-    #writer.writeheader() # uses fieldnames set above
-    #writer.writerow({"city": "New York", "name": "Yankees"})
-    #writer.writerow({"city": "New York", "name": "Mets"})
-    #writer.writerow({"city": "Boston", "name": "Red Sox"})
-    #writer.writerow({"city": "New Haven", "name": "Ravens"})
-
-# Read CSV File
-#csv_file_path = "teams.csv"
-
-#with open(csv_file_path, "r") as csv_file:
-    #reader = csv.DictReader(csv_file) # assuming your CSV has headers, otherwise... csv.reader(csv_file)
-    #for row in reader:
-        #print(row["city"], row["name"])
+with open(csv_file_path,"r") as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        print(" + " + str(row))
 
 
-
-
-
-#Project Requirements
-#Contains a README.md file which provides instructions for how to install and use the program.
-#Contains a app/products_app.py file which represents the Python program.
-#Contains an empty data directory except for presence of a data/.gitignore file (see setup instructions).
-#Contains a commit history which reflects incremental development progress, perhaps corresponding with the checkpoint steps.
+#while True:
+    #operation = input ("Please input a valid operation")
