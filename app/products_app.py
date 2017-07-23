ed07a510d8d50aa7e50ca63bd87ff4ea9d985377
Alberts <jalberts@deloitte.com>
2017-07-23 11:49:40 -0400

    Reading & Writing to CSV

--- app/products_app.py
+++ app/products_app.py
@@ -4,26 +4,14 @@ csv_file_path_2 = "data/other_products.csv"
 
 products = []
 
-#List File
-def read_file():
-    with open(csv_file_path,"r") as csv_file:
-        reader = csv.DictReader(csv_file)
-        for row in reader:
-            print(" + ", dict(row))
+csv_file_path = "data/products.csv"
+csv_file_path_2 = "data/other_products.csv"
 
-#Read & Append
-with open(csv_file_path,"r") as csv_file:
+# READ PRODUCTS CSV
+with open(csv_file_path, "r") as csv_file:
     reader = csv.DictReader(csv_file)
     for row in reader:
         products.append(row)
-
-#Write File
-def write_file():
-    with open(csv_file_path_2, "w") as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=["id", "name", "aisle", "department", "price"])
-        writer.writeheader() # uses fieldnames set above
-        for product in products:
-            writer.writerow(product)
 
 #Menu Greeting & Inputs
 print("----------------------------------")
@@ -46,32 +34,47 @@ chosen_operation = input("Input Operation: ")
 chosen_operation = chosen_operation.title()
 print(chosen_operation)
 
-
-
 #Menu Definitions
-def list_products(): #List Definition #TODO Troubleshoot
-        print("\n")
-        print("List")
-        print("THERE ARE 20 PRODUCTS:")
-        read_file()
+def list_products():
+    print("LISTING PRODUCTS")
 
-def show_product(): #Show Definition
-    #print("SHOWING A PRODUCT")
-    print("There are " + str(len(products)) + " Products")
+def show_product():
+    print("SHOWING A PRODUCT")
 
-def create_product(): #Create Definition
-    write_file()
+def create_product():
+    print("CREATING A PRODUCT")
+    product_name = input("name is:")
+    product_aisle = input("aisle is:")
+    product_department = input("department is:")
+    product_price = input("price is:")
+    new_product = {
+        "id": len(products) + 1,
+        "name": product_name,
+        "aisle": product_aisle,
+        "department": product_department,
+        "price": product_price
+    }
+    print("NEW PRODUCT IS", new_product)
+    products.append(new_product)
 
-def update_product(): #Update Definition
+def update_product():
     print("UPDATING A PRODUCT")
 
-def destroy_product(): #Destroy Definition
+def destroy_product():
     print("DESTROYING A PRODUCT")
 
-#Menu If statements
+#Menu If Statements
 if chosen_operation == "List": list_products()
 elif chosen_operation == "Show": show_product()
 elif chosen_operation == "Create": create_product()
 elif chosen_operation == "Update": update_product()
 elif chosen_operation == "Destroy": destroy_product()
 else: print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.")
+
+# OVERWRITING INVENTORY CSV FILE
+
+with open(csv_file_path_2, "w") as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=["id","name","aisle","department","price"])
+    writer.writeheader() # uses fieldnames set above
+    for product in products:
+        writer.writerow(product)
